458fdbd453b11c4b82483d960585781e5b8e7190
MoritzFreymann <32904684+MoritzFreymann@users.noreply.github.com>
2017-11-01 20:24:09 +0100

    GUI und Ablauf Timer

--- Jump-Twister.js
+++ Jump-Twister.js
@@ -1,22 +1,23 @@
 'use strict';
 //Variablen fuer die Eingabe
-var dauer = '000';
+var dauer = 0;
 var korrektedauer = 0;
-var T = '0';
-
-//GoOnbutton
-var weiter = true;
-var showGoOn =true;
-//Screen
+var T = 0;
+var Kommandotyp_Fuesse = 'leer';
+// Timer
+var currenttime = 0;
+var starttime = 0;
+var lasttime = 0;
+// Screen
 var screenheight = 0;
 var screenwidth = 0;
 
-//Ausslesen der Bildschirmgroesse
+// Ausslesen der Bildschirmgroesse
 Screen();
 
-//Funktionen
+// Funktionen
 
-//Bildschirmgroesse bestimmen
+// Bildschirmgroesse bestimmen
 function Screen(){
 	screenwidth = screen.width;
 	screenheight = screen.height;
@@ -52,25 +53,50 @@ function GetRightPosition(){
 }
 //
 */
-//EndInput
+// EndInput
 function EndInput(){
 	var d = document.getElementById('Input');
 	d.style.zIndex = 1;
 	var d = document.getElementById('HintergrundUebung');
 	d.style.zIndex = 10;
-	var d = document.getElementById('rotesQuadrat');
+	var d = document.getElementById('Quadrat_linker_Fuss');
 	d.style.zIndex = 20;
 	
-}
-//
-//Setze Playmode
-function SetPlaymode(){
-
-}
-//
-
-//
-function NextGame(){
+	//
+	getvalues();
 	
+	// Startzeit in Sekunden
+	var date = new Date();
+	starttime = date.getHours() *3600 + date.getMinutes() *60 + date.getSeconds();
+	
+	if (Kommandotyp_Fuesse == 'Zahlen'){
+		var d = document.getElementById('Quadrat_linker_Fuss');
+		d.style.backgroundColor = 'green';
+	}
+	
+	
+}
+//
+// Do
+function Do(){
+		
+	while( currenttime - starttime < dauer ){	// pruefen, ob Dauer noch nicht überschritten
+		
+	// get current time
+	var date = new Date();
+	currenttime = date.getHours() *3600 + date.getMinutes() *60 + date.getSeconds();
+	
+		if( currenttime - lasttime > T ){
+			lasttime = currenttime;	// letzte Ausfuehrungszeitpunkt ist aktueller Ausfuehrungszeitpunkt
+		}
+	}
+}
+//
+
+//
+function getvalues(){
+	dauer = document.getElementById("dauer").value;
+	T = document.getElementById("T").value;
+	Kommandotyp_Fuesse = document.getElementById("zahlen").value;
 }
 //
